47299255dd1ff7ae2294f73550403e2c37a757d2
Shahidul Islam <ishahin56@gmail.com>
2023-07-18 15:44:10 +0600

    Created jwt token using firebase/php-jwt library

---
+++ app/Helper/JWTToken.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Helper;
+use Exception;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JWTToken {
+    function CreateToken($userEmail):string{
+        $key = env('JWT_KEY');
+        $payload = [
+            'iss' => 'laravel-token',
+            // 'aud' => 'http://example.com',
+            'iat' => time(),
+            'exp' => time()+60*60,
+            'userEmail' => $userEmail
+        ];
+
+        return JWT::encode($payload, $key, 'HS256');
+    }
+
+    function VerifyToken($token):string{
+        try{
+            $key = env('JWT_KEY');
+            $decoded = JWT::decode($token, new Key($key, 'HS256'));
+            return $decoded->userEmail;
+        }
+        catch(Exception $e){
+            return "Unauthorized";
+        }
+    }
+}
